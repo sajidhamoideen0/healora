1726d5680e8fa12b4b618bb6c7c3b183a947e8fc
Sajidha <sajidhamoideen0@gmail.com>
2026-06-30 18:35:06 +0530

    Add CSRF trusted origins

--- skin_clinic/settings.py
+++ skin_clinic/settings.py
@@ -93,12 +93,5 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = os.getenv('EMAIL_HOST')
-# EMAIL_PORT = os.getenv('EMAIL_PORT')
-# EMAIL_USE_TLS = True
-# EAMIL_USE_SSL = False
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-# DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
+CSRF_TRUSTED_ORIGINS = ['https://sajidhamoideen.pythonanywhere.com']
